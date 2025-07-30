53e1471a3843fd93bb401250e8fa4d9a7ed94bc8
Waqas Khalid <mwaqaskhalid1662@gmail.com>
2025-07-30 14:18:23 -0400

    sign up api

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,51 @@
-export const signup = (req, res) => {
-    res.send("signup route");
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+
+import {generateToken} from "../lib/utils.js"
+
+export const signup = async (req, res) => {
+    console.log("in auth contorller");
+    const { fullName, email, password} = req.body;
+    console.log(fullName);
+    try {
+        if(password.length < 6){
+            return res.status(400).json({message: "Password must be at least 6 characters"});
+        }
+
+        const user = await User.findOne({email});
+
+        if(user) return res.status(400).json({message: "Email already exists"});
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            fullName,
+            email,
+            password: hashedPassword,
+        })
+
+        if(newUser){
+            //generate jwt token here
+            generateToken(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+            });
+
+        }
+        else{
+            res.status(400).json({message: "Invalid User data"});
+        }
+
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 };
 
 export const login = (req, res) => {
--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,10 @@ import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 const PORT = process.env.PORT;
 
+
 const app = express();
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
