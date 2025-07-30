01de38c3726ba59506f5049c0f6b08b44e1f02d4
Waqas Khalid <mwaqaskhalid1662@gmail.com>
2025-07-30 11:41:13 -0400

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+import {connectDB} from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+    console.log("server is running on port: ", PORT);
+    connectDB();
+});
